bc17dde9a882dee4a5278e553fd6722dbc0ed694
stammaprang <yanning-1987@163.com>
2026-06-02 17:06:41 +0800

    initial

--- yan-crypto-payment-eth/src/main/java/com/yan/blockchain/pay/eth/task/GenAddressTask.java
+++ yan-crypto-payment-eth/src/main/java/com/yan/blockchain/pay/eth/task/GenAddressTask.java
@@ -40,8 +40,8 @@ public class GenAddressTask {
 	public void run(){
 		chainConfigs.keySet().forEach(chain -> {
 			Long count = addressPoolMapper.selectCount(new LambdaQueryWrapper<AddressPool>().eq(AddressPool::getChain, chain).eq(AddressPool::getUsed, AddressStatus.UNUSED));
-			if( count != null && count >= 19) return;
-			for (int i = 0; i < 20; i++) {
+			if( count != null && count >= 50) return;
+			for (int i = 0; i < 100; i++) {
 				createTronAccount(chain);
 			}
 		});
